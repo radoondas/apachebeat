8827230f6e7342709873bb941546ca5ebdd1fb6a
Rado Ondas <ondas.radovan@gmail.com>
2016-01-29 22:12:14 +0100

    Fixes #10

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"github.com/elastic/beats/libbeat/beat"
 
 	apachebeat "github.com/radoondas/apachebeat/beat"
+	"os"
 )
 
 var Version = "1.0.0-beta2"
 var Name = "apachebeat"
 
 func main() {
-
-	beat.Run(Name, Version, apachebeat.New())
+	err := beat.Run(Name, Version, apachebeat.New())
+	if err != nil {
+		os.Exit(1)
+	}
 }
