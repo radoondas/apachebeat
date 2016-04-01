1c2e1d85aeea9727a2dbc8061ad9569046e38c1d
Rado Ondas <ondas.radovan@gmail.com>
2016-04-01 12:05:44 +0200

    Implement BasicAuth for server status requests

--- beater/config.go
+++ beater/config.go
@@ -8,6 +8,12 @@ type ApacheConfig struct {
 	// Period defines how often to read status in seconds.
 	// Defaults to 1 second.
 	Period *int64
+
+	// Authentication for BasicAuth
+	Authentication struct {
+		Username *string
+		Password *string
+	}
 }
 
 type ConfigSettings struct {
